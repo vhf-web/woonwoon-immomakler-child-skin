acf36673a62c5e756838902167f8590671868e7e
vhf <vhf@re5800.dk>
2026-02-11 13:24:23 +0100

    All fix

---
+++ archive-immomakler_object.php
@@ -0,0 +1,5 @@
+<?php immomakler_get_template_part('header', 'archive'); ?>
+
+<?php immomakler_get_template_part('archive/archive', 'property'); ?>
+
+<?php immomakler_get_template_part('footer', 'archive'); ?>
---
+++ single-immomakler_object.php
@@ -0,0 +1,5 @@
+<?php immomakler_get_template_part( 'header', 'single' ); ?>
+
+<?php immomakler_get_template_part( 'single/single', 'property' ); ?>
+
+<?php immomakler_get_template_part( 'footer', 'single' );
