66c0075fd6d677c44fd794cac317a5171f5181a4
vhf <vhf@re5800.dk>
2026-02-12 10:04:16 +0100

    Add Debug

--- single-immomakler_object.php
+++ single-immomakler_object.php
@@ -1,4 +1,8 @@
-<?php immomakler_get_template_part( 'header', 'single' ); ?>
+<?php
+// Debug: verify that immomakler-child-skin single template is used.
+echo 'CHILD SKIN SINGLE TEMPLATE';
+immomakler_get_template_part( 'header', 'single' );
+?>
 
 <?php immomakler_get_template_part( 'single/single', 'property' ); ?>
 
