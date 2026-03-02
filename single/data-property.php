79ed00123ef255f54b47d4600514352b3411a38a
vhf <vhf@re5800.dk>
2026-03-02 10:18:39 +0100

    Objekt ID not translated

--- single/data-property.php
+++ single/data-property.php
@@ -1,14 +1,9 @@
 <?php
 /**
  * Objektdaten panel – child override.
- * Adds translate="no" and class "notranslate" for Objekt-ID and address/street
- * so browser/plugin automatic translation does not translate them.
+ * Wraps Objekt-ID value in <span class="no-translate"> so it is not auto-translated.
  */
 $property_data = new \ImmoMakler\Data\PropertyData();
-
-// Keys that must not be auto-translated (Objekt-ID, address/street).
-$no_translate_keys = apply_filters( 'immomakler_single_data_notranslate_keys', [ 'objektnr_extern', 'adresse' ] );
-$no_translate_keys = is_array( $no_translate_keys ) ? $no_translate_keys : [ 'objektnr_extern', 'adresse' ];
 ?>
 
 <div class="property-details panel panel-default">
@@ -18,18 +13,16 @@ $no_translate_keys = is_array( $no_translate_keys ) ? $no_translate_keys : [ 'ob
 
 		<?php foreach ( $property_data->get_single_keys() as $key ) : ?>
 			<?php if ( $property_data->show( $key ) ) : ?>
-			<?php
-			$no_translate = in_array( $key, $no_translate_keys, true );
-			$li_class     = 'list-group-item data-' . sanitize_key( $key );
-			if ( $no_translate ) {
-				$li_class .= ' notranslate';
-			}
-			$li_attr = $no_translate ? ' translate="no"' : '';
-			?>
-			<li class="<?php echo esc_attr( $li_class ); ?>"<?php echo $li_attr; ?>>
+			<li class="list-group-item data-<?php echo sanitize_key( $key ); ?>">
 				<div class="row">
 					<div class="dt col-sm-5"><?php echo esc_html( $property_data->get_label( $key ) ); ?></div>
-					<div class="dd col-sm-7"><?php echo wp_kses_post( $property_data->get_value( $key ) ); ?></div>
+					<div class="dd col-sm-7"><?php
+					if ( $key === 'objektnr_extern' ) {
+						echo '<span class="no-translate">' . wp_kses_post( $property_data->get_value( $key ) ) . '</span>';
+					} else {
+						echo wp_kses_post( $property_data->get_value( $key ) );
+					}
+					?></div>
 				</div>
 			</li>
 			<?php endif; ?>
@@ -39,15 +32,7 @@ $no_translate_keys = is_array( $no_translate_keys ) ? $no_translate_keys : [ 'ob
 
 		<?php foreach ( $property_data->get_single_price_keys() as $key ) : ?>
 			<?php if ( $property_data->show( $key ) ) : ?>
-			<?php
-			$no_translate = in_array( $key, $no_translate_keys, true );
-			$li_class     = 'list-group-item data-' . sanitize_key( $key );
-			if ( $no_translate ) {
-				$li_class .= ' notranslate';
-			}
-			$li_attr = $no_translate ? ' translate="no"' : '';
-			?>
-			<li class="<?php echo esc_attr( $li_class ); ?>"<?php echo $li_attr; ?>>
+			<li class="list-group-item data-<?php echo sanitize_key( $key ); ?>">
 				<div class="row price">
 					<div class="dt col-sm-5"><?php echo esc_html( $property_data->get_label( $key ) ); ?></div>
 					<div class="dd col-sm-7"><?php echo wp_kses_post( $property_data->get_value( $key ) ); ?></div>
